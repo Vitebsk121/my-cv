97dbec523e04a90adf3944933a22f0ca45c0ab93
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-11 15:26:00 +0300

    feat: added nav

--- src/Components/Navigation/Drawer/Drawer.tsx
+++ src/Components/Navigation/Drawer/Drawer.tsx
@@ -1,34 +1,18 @@
 import './Drawer.scss';
 
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import useTypedSelector from '../../../hooks/useTypedSelector';
-import { closeMenu, openMenu } from '../../../store/actions/appActions';
+import Nav from '../Nav/Nav';
 
 type DrawerProps = {};
 
 const Drawer: React.FC<DrawerProps> = () => {
-  const dispatch = useDispatch();
   const { menuIsOpen } = useTypedSelector((state) => state.appState);
-
-  const menuHandler = () => {
-    dispatch(menuIsOpen ? closeMenu() : openMenu());
-  };
 
   return (
     <div className={menuIsOpen ? 'drawer open' : 'drawer'}>
-      <nav className="navMenu">
-        <ul className="navList">
-          <li role="none" onClick={menuHandler} className="navItem">
-            <Link to="/">CV</Link>
-          </li>
-          <li role="none" onClick={menuHandler} className="navItem">
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+      <Nav />
     </div>
   );
 };
