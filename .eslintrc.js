099c6e6179bfade7529983d1c01f7a2b49687dea
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-11 13:08:55 +0300

    fix: max-len up to 120

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   plugins: ['simple-import-sort', 'prettier'],
   rules: {
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'max-len': '120',
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/accessible-emoji': 'off',
     'react/prop-types': 'off',
