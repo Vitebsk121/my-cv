2c52a83324ef21e9dfc65cb23a40554ec8136694
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-11 21:55:04 +0300

    feat: added page 404

---
+++ src/containers/Pages/ErrorPage404/ErrorPage404.tsx
@@ -0,0 +1,13 @@
+import './ErrorPage404.scss';
+
+import React from 'react';
+
+type ErrorPage404Props = {};
+
+const ErrorPage404: React.FC<ErrorPage404Props> = () => (
+  <div className="ErrorPage404">
+    <h1>404</h1>
+  </div>
+);
+
+export default ErrorPage404;
