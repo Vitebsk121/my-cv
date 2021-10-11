2a06e61c2f8036592b480e902a42d458799c7fc5
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-11 21:56:20 +0300

    refactor: refactor router

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,26 +1,35 @@
 import './App.scss';
 
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import WrapperLayout from '../../Components/HOC/wrapperLayout';
 import useTypedSelector from '../../hooks/useTypedSelector';
 import AboutPage from '../Pages/AboutPage/AboutPage';
 import CVPage from '../Pages/CVPage/CVPage';
+import ErrorPage404 from '../Pages/ErrorPage404/ErrorPage404';
 
 const App = () => {
   const { theme } = useTypedSelector((state) => state.appState);
+
+  const routes = [
+    { path: '/cv', exact: true, Component: CVPage },
+    { path: '/about', exact: true, Component: AboutPage },
+  ];
 
   return (
     <div className={['app', theme].join(' ')}>
       <BrowserRouter>
         <WrapperLayout>
-          <Route path="/" exact>
-            <CVPage />
-          </Route>
-          <Route path="/about">
-            <AboutPage />
-          </Route>
+          <Switch>
+            {routes.map(({ exact, path, Component }) => (
+              <Route key={path} path={path} exact={exact}>
+                {() => <Component />}
+              </Route>
+            ))}
+            <Redirect from="/" to="/cv" exact />
+            <Route component={ErrorPage404} />
+          </Switch>
         </WrapperLayout>
       </BrowserRouter>
     </div>
