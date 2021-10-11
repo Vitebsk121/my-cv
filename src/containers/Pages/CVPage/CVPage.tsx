3aa76add72e588e8c36750c63bda62feab7cf08e
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-11 21:54:42 +0300

    feat: added avatar

--- src/containers/Pages/CVPage/CVPage.tsx
+++ src/containers/Pages/CVPage/CVPage.tsx
@@ -2,6 +2,7 @@ import './CVPage.scss';
 
 import React from 'react';
 
+import avatar from '../../../../public/images/Avatar.jpeg';
 import PageTitleLayout from '../../../Components/HOC/pageTitleLayout';
 
 type CVPageProps = {};
@@ -9,9 +10,16 @@ type CVPageProps = {};
 const CVPage: React.FC<CVPageProps> = () => (
   <PageTitleLayout title="CV">
     <div className="cvPage">
-      <div className="gridItem item1">1</div>
-      <div className="gridItem item2">2</div>
-      <div className="gridItem item3">3</div>
+      <div className="gridItem item1">
+        <img className="cv__avatar" src={avatar} alt="asd" />
+      </div>
+      <div className="gridItem item2">
+        <h3 className="cv__title">Alexandr Demchenko</h3>
+        <h4 className="cv__subTitle">Junior Software Developer</h4>
+      </div>
+      <div className="gridItem item3">
+        <h3 className="cv__title">Contacts:</h3>
+      </div>
       <div className="gridItem item4">4</div>
       <div className="gridItem item5">5</div>
       <div className="gridItem item6">6</div>
