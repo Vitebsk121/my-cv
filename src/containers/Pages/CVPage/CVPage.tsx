4e363ae2bbb85894f738e37bf55ca62439d72858
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-11 15:09:13 +0300

    feat: added CV page

--- src/containers/Pages/CVPage/CVPage.tsx
+++ src/containers/Pages/CVPage/CVPage.tsx
@@ -9,7 +9,16 @@ type CVPageProps = {};
 const CVPage: React.FC<CVPageProps> = () => (
   <PageTitleLayout title="CV">
     <div className="cvPage">
-      <h1>CV PAGE</h1>
+      <div className="gridItem item1">1</div>
+      <div className="gridItem item2">2</div>
+      <div className="gridItem item3">3</div>
+      <div className="gridItem item4">4</div>
+      <div className="gridItem item5">5</div>
+      <div className="gridItem item6">6</div>
+      <div className="gridItem item7">7</div>
+      <div className="gridItem item8">8</div>
+      <div className="gridItem item9">9</div>
+      <div className="gridItem item10">10</div>
     </div>
   </PageTitleLayout>
 );
