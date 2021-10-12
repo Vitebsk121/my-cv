2013734fc4d1eb36198ed3774212c1f9c82c44c3
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-12 11:50:40 +0300

    feat: added routes

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import AboutPage from '../containers/Pages/AboutPage/AboutPage';
+import CVPage from '../containers/Pages/CVPage/CVPage';
+
+const routes = [
+  { path: '/cv', exact: true, component: CVPage },
+  { path: '/about', exact: true, component: AboutPage },
+  // { path: '/projects', exact: true, component: ProjectsPage },
+  // { path: '/projects/:id', exact: true, component: ProjectIDPage },
+];
+
+export default routes;
