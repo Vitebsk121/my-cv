7fb9b2bd5f329fdf6fde3b33000842b64a282b78
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-15 13:33:48 +0300

    feat: added contactsCV

---
+++ src/Components/Contacts/Contacts.tsx
@@ -0,0 +1,46 @@
+import './Contacts.scss';
+
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import HomeWorkIcon from '@mui/icons-material/HomeWork';
+import React from 'react';
+
+import useTypedSelector from '../../hooks/useTypedSelector';
+
+type ContactsProps = {};
+
+const Contacts: React.FC<ContactsProps> = () => {
+  const { theme } = useTypedSelector((state) => state.appState);
+  return (
+    <>
+      <h3 className="title">Contacts</h3>
+      <h4 className="title">
+        <a className="subtitle link" href="tel:+79778480811">
+          <PhoneIphoneIcon fontSize="small" color={theme === 'lightTheme' ? 'primary' : 'action'} />
+          <p className="link-title">+7 (977) 848-08-11</p>
+        </a>
+      </h4>
+      <h4 className="title">
+        <a className="subtitle link" href="mailto:Alexandr.Demchenko@list.ru">
+          <AlternateEmailIcon fontSize="small" color={theme === 'lightTheme' ? 'primary' : 'action'} />
+          <p className="link-title">Alexandr.Demchenko@list.ru</p>
+        </a>
+      </h4>
+      <h4 className="title">
+        <a className="subtitle link" rel="noreferrer" target="_blank" href="https://t.me/Alexandr_Demchenko">
+          <TelegramIcon fontSize="small" color={theme === 'lightTheme' ? 'primary' : 'action'} />
+          <p className="link-title">Alexandr Demchenko</p>
+        </a>
+      </h4>
+      <h4 className="title">
+        <h3 className="subtitle link">
+          <HomeWorkIcon fontSize="small" color={theme === 'lightTheme' ? 'primary' : 'action'} />
+          <p className="link-title">Moscow</p>
+        </h3>
+      </h4>
+    </>
+  );
+};
+
+export default Contacts;
