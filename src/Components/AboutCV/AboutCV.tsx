bff649dcbdb9cb1de7be05fdf5d8ac54f7db0561
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-15 13:34:05 +0300

    feat: added aboutCV

---
+++ src/Components/AboutCV/AboutCV.tsx
@@ -0,0 +1,25 @@
+import './AboutCV.scss';
+
+import React from 'react';
+
+const AboutCV: React.FC = () => (
+  <>
+    <h3 className="title">About</h3>
+    <h4 className="subtitle">
+      Hi! My name is Alexandr. I was born in Belarus where I started to make myself a career. Some time later moved to
+      Russia. Nowadays I work for the international nicotine company British American Tobacco Russia. While working I
+      proved to be one of the 8 best workers (out of 100 workers on the similar vacation in Moscow.
+    </h4>
+    <h4 className="subtitle">
+      Interesting projects, possibility of personal and careers development , the team itself - that’s what keeps me
+      motivated. I really like to optimize everything; to find out the mistakes, analyze numbers and provide solutions.
+    </h4>
+    <h4 className="subtitle">Development areas: fear of flights, English level.</h4>
+    <h4 className="subtitle">
+      I’m moderately hard-working ( prefer work and life balance theory ) at present I devote much time to my family and
+      self-development. Love traveling with family and friends to Europe.
+    </h4>
+  </>
+);
+
+export default AboutCV;
