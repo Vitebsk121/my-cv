b41c59610ce425cae576d3443316cfae6de8aad8
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-15 13:32:34 +0300

    feat: added SkillsCV

---
+++ src/Components/SkillsCV/SkillsCV.tsx
@@ -0,0 +1,51 @@
+import './SkillsCV.scss';
+
+import React from 'react';
+
+type SkillsCVProps = {};
+
+const SkillsCV: React.FC<SkillsCVProps> = () => (
+  <>
+    <h3 className="title">Skills</h3>
+    <ul className="skillsList">
+      <li>
+        <h4 className="subtitle">JavaScrypt</h4>
+      </li>
+      <li>
+        <h4 className="subtitle">TypeScrypt</h4>
+      </li>
+      <li>
+        <h4 className="subtitle">HTML</h4>
+      </li>
+      <li>
+        <h4 className="subtitle">CSS</h4>
+      </li>
+      <li>
+        <h4 className="subtitle">SCSS</h4>
+      </li>
+      <li>
+        <h4 className="subtitle">WebPack</h4>
+      </li>
+      <li>
+        <h4 className="subtitle">Vite</h4>
+      </li>
+      <li>
+        <h4 className="subtitle">React</h4>
+      </li>
+      <li>
+        <h4 className="subtitle">Redux</h4>
+      </li>
+      <li>
+        <h4 className="subtitle">Git</h4>
+      </li>
+      <li>
+        <h4 className="subtitle">JEST</h4>
+      </li>
+      <li>
+        <h4 className="subtitle">React Testing library</h4>
+      </li>
+    </ul>
+  </>
+);
+
+export default SkillsCV;
