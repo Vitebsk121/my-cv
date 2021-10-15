5ba7b620d34ef1003c380b24c1f22ba67bd2c333
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-15 13:33:08 +0300

    feat: added gitHub link to footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -4,6 +4,11 @@ import React from 'react';
 
 type FooterProps = {};
 
-const Footer: React.FC<FooterProps> = () => <div className="footer" />;
+const Footer: React.FC<FooterProps> = () => <div className="footer">
+  <a target="_blank" href="https://github.com/vitebsk121" className='contactLink'>
+    <div className="contactIcon" />
+    Alexandr Demchenko
+  </a>
+</div>;
 
 export default Footer;
