ac01ba2fdb0e95b2cd46d08ab8416ca8c3d24581
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-15 13:33:24 +0300

    feat: added experienceCV

---
+++ src/Components/ExperienceCV/ExperienceCV.tsx
@@ -0,0 +1,40 @@
+import "./ExperienceCV.scss";
+
+import React from "react";
+
+const ExperienceCV: React.FC = () => {
+  return (
+
+    <>
+      <h3 className="title">Experience</h3>
+      <ul className="educationList">
+        <li>
+          <h4 className="subtitle bold">British American Tobacco Russia</h4>
+          <ul className='educationSubList'>
+            <li>
+              <h4 className="subtitle">Trade marketing representative. 2016 to present</h4>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h4 className="subtitle bold">Эколайнбел</h4>
+          <ul className='educationSubList'>
+            <li>
+              <h4 className="subtitle">Торговый представитель. 2015-2016</h4>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h4 className="subtitle bold">British american tobacco trading company</h4>
+          <ul className='educationSubList'>
+            <li>
+              <h4 className="subtitle">Коммерческий агент. 2014-2015</h4>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default ExperienceCV;
