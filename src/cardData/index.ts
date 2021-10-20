080a67e385d93d92a13859e5adb1b3f84d869584
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-20 22:38:14 +0300

    feat: added project list (test)

---
+++ src/cardData/index.ts
@@ -0,0 +1,27 @@
+import asyncRaceImg from '../../public/images/asyncRace.png';
+import matchMatchGameImg from '../../public/images/matchMatchGame.png';
+import photoFilterImg from '../../public/images/photoFilter.png';
+import virtualPianoImg from '../../public/images/virtualPiano.png';
+
+export const arrayOfProjects = [
+  {
+    imageURL: virtualPianoImg,
+    title: 'Virtual piano',
+    shortDescription: 'trattata tatat atata tatat tata ',
+  },
+  {
+    imageURL: photoFilterImg,
+    title: 'Photo filter',
+    shortDescription: 'trattata tatat atata tatat tata ',
+  },
+  {
+    imageURL: matchMatchGameImg,
+    title: 'Match match game',
+    shortDescription: 'trattata tatat atata tatat tata ',
+  },
+  {
+    imageURL: asyncRaceImg,
+    title: 'Async Race',
+    shortDescription: 'trattata tatat atata tatat tata ',
+  },
+];
