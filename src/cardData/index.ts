4499e724a92b8a156ac4793bfe49250685af0a5b
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-22 12:40:09 +0300

    feat: added new projects

--- src/cardData/index.ts
+++ src/cardData/index.ts
@@ -1,27 +1,51 @@
 import asyncRaceImg from '../../public/images/asyncRace.png';
+import englishForKidsImg from '../../public/images/englishForKids.png';
 import matchMatchGameImg from '../../public/images/matchMatchGame.png';
+import onlineZooImg from '../../public/images/onlineZoo.png';
 import photoFilterImg from '../../public/images/photoFilter.png';
+import planningPokerImg from '../../public/images/planningPoker.png';
+import reactAppImg from '../../public/images/reactApp.png';
 import virtualPianoImg from '../../public/images/virtualPiano.png';
 
 export const arrayOfProjects = [
   {
     imageURL: virtualPianoImg,
     title: 'Virtual piano',
-    shortDescription: 'trattata tatat atata tatat tata ',
+    shortDescription: 'HTML CSS JS',
   },
   {
     imageURL: photoFilterImg,
     title: 'Photo filter',
-    shortDescription: 'trattata tatat atata tatat tata ',
+    shortDescription: 'HTML CSS JS',
   },
   {
     imageURL: matchMatchGameImg,
     title: 'Match match game',
-    shortDescription: 'trattata tatat atata tatat tata ',
+    shortDescription: 'TS WebPack SCSS IndexedDB LocalStorage',
   },
   {
     imageURL: asyncRaceImg,
     title: 'Async Race',
-    shortDescription: 'trattata tatat atata tatat tata ',
+    shortDescription: 'TS WebPack SCSS API',
+  },
+  {
+    imageURL: onlineZooImg,
+    title: 'Online Zoo',
+    shortDescription: 'HTML CSS JS',
+  },
+  {
+    imageURL: englishForKidsImg,
+    title: 'English for kids',
+    shortDescription: 'TS WebPack SCSS',
+  },
+  {
+    imageURL: reactAppImg,
+    title: 'React App',
+    shortDescription: 'React TS Vite SCSS Axios Jest React Testing Library React Transition Group',
+  },
+  {
+    imageURL: planningPokerImg,
+    title: 'Planning Poker',
+    shortDescription: 'Team Task React TS WebPack SCSS Semantic UI API Next Nest Postgres',
   },
 ];
