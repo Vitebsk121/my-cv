4a462cf72901db8d614b92e76b30ee655e85c5f4
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-12-01 08:54:12 +0300

    feat: added projectCard

--- src/Components/ProjectCard/ProjectCard.tsx
+++ src/Components/ProjectCard/ProjectCard.tsx
@@ -6,10 +6,23 @@ type ProjectCardProps = {
   imageURL: string;
   title: string;
   shortDescription: string;
+  id: number;
+  setCurrentProjectID: React.Dispatch<React.SetStateAction<number>>;
 };
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ imageURL, title, shortDescription }: ProjectCardProps) => (
-  <figure className="projectCard" style={{ backgroundImage: `url(${imageURL})` }}>
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  imageURL,
+  title,
+  shortDescription,
+  id,
+  setCurrentProjectID,
+}: ProjectCardProps) => (
+  <figure
+    role="none"
+    className="projectCard"
+    style={{ backgroundImage: `url(${imageURL})` }}
+    onClick={() => setCurrentProjectID(id)}
+  >
     <figcaption className="cardTitle">{title}</figcaption>
     <figcaption className="cardDescription">{shortDescription}</figcaption>
   </figure>
